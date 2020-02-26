b65b0bcc0516ce1054e86adf6adfb5bd9fe4880f
Voldracarno Draconor <thafiredragonofdeath@gmail.com>
2020-02-26 11:42:00 +0100

    Work on config TOML parsing

--- src/config.rs
+++ src/config.rs
@@ -18,11 +18,34 @@
 extern crate toml;
 
 use toml::{Value, de::Error};
+use std::convert::TryFrom;
+use crate::config::ConnectionMethod::{Tcp, Udp, UnixSocket};
 
+#[derive(Copy, Clone)]
 enum ConnectionMethod {
     Tcp,
     Udp,
     UnixSocket,
+}
+
+impl TryFrom<&str> for ConnectionMethod {
+    type Error = &'static str;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let value_lowercase = value.to_lowercase();
+
+        if value_lowercase == "tcp" {
+            return Ok(Tcp);
+        }
+        else if value_lowercase == "udp" {
+            return Ok(Udp);
+        }
+        else if value_lowercase == "unixsocket" {
+            return Ok(UnixSocket);
+        }
+
+        return Err("Failed to parse connection method");
+    }
 }
 
 struct ConfigField<T> {
@@ -120,6 +143,28 @@ struct NetworkConfig {
     port: ConfigField<u16>,
 }
 
+impl NetworkConfig {
+    pub fn new() -> NetworkConfig {
+        NetworkConfig {
+            ip_addr: ConfigField::new_empty(String::new()),
+            port: ConfigField::new_empty(0),
+        }
+    }
+
+    pub fn parse_toml(&mut self, toml_obj: &Value) {
+        let ip_addr = toml_obj["NetworkConfig"]["ip_addr"].as_str();
+        let port = toml_obj["NetworkConfig"]["port"].as_integer();
+
+        if ip_addr.is_some() {
+            self.ip_addr.set_value(ip_addr.unwrap().to_owned());
+        }
+
+        if port.is_some() {
+            self.port.set_value(port.unwrap() as u16);
+        }
+    }
+}
+
 struct PostgresConfig {
     host: ConfigField<String>,
     port: ConfigField<u16>,
@@ -129,6 +174,68 @@ struct PostgresConfig {
     password: ConfigField<String>,
     db_name: ConfigField<String>,
     required_schema_version: ConfigField<u32>,
+}
+
+impl PostgresConfig {
+    pub fn new() -> PostgresConfig {
+        PostgresConfig {
+            host: ConfigField::new_empty(String::new()),
+            port: ConfigField::new_empty(0),
+            unix_socket_dir: ConfigField::new_empty(String::new()),
+            connection_method: ConfigField::new_empty(Tcp),
+            user: ConfigField::new_empty(String::new()),
+            password: ConfigField::new_empty(String::new()),
+            db_name: ConfigField::new_empty(String::new()),
+            required_schema_version: ConfigField::new_empty(0),
+        }
+    }
+
+    pub fn parse_toml(&mut self, toml_obj: &Value) {
+        let host = toml_obj["PostgresConfig"]["host"].as_str();
+        let port = toml_obj["PostgresConfig"]["port"].as_integer();
+        let unix_socket_dir = toml_obj["PostgresConfig"]["unix_socket_dir"].as_str();
+        let connection_method = toml_obj["PostgresConfig"]["connection_method"].as_str();
+        let user = toml_obj["PostgresConfig"]["user"].as_str();
+        let password = toml_obj["PostgresConfig"]["password"].as_str();
+        let db_name = toml_obj["PostgresConfig"]["db_name"].as_str();
+        let required_schema_version = toml_obj["PostgresConfig"]["required_schema_version"].as_integer();
+
+        if host.is_some() {
+            self.host.set_value(host.unwrap().to_owned());
+        }
+
+        if port.is_some() {
+            self.port.set_value(port.unwrap() as u16);
+        }
+
+        if unix_socket_dir.is_some() {
+            self.unix_socket_dir.set_value(unix_socket_dir.unwrap().to_owned());
+        }
+
+        if connection_method.is_some() {
+            let connection_method_str = connection_method.unwrap();
+            let connection_method_obj = ConnectionMethod::try_from(connection_method_str);
+            if connection_method_obj.is_ok() {
+                self.connection_method.set_value(connection_method_obj.unwrap());
+            }
+        }
+
+        if user.is_some() {
+            self.user.set_value(user.unwrap().to_owned());
+        }
+
+        if password.is_some() {
+            self.password.set_value(password.unwrap().to_owned());
+        }
+
+        if db_name.is_some() {
+            self.db_name.set_value(db_name.unwrap().to_owned());
+        }
+
+        if required_schema_version.is_some() {
+            self.required_schema_version.set_value(required_schema_version.unwrap() as u32);
+        }
+    }
 }
 
 struct RedisConfig {
