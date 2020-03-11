e0e33b1fac15246f7578246bf2c86eff81eaeb1c
Voldracarno Draconor <thafiredragonofdeath@gmail.com>
2020-03-11 20:30:15 +0100

    Implement upload data sql command

---
+++ ressources/sql/Schema-v1/get_upload_data.sql
@@ -0,0 +1,8 @@
+-- Input: upload_id
+-- Returns: upload_filename; upload_timestamp; is_nsfw, uploader_id, uploader_username, upvotes
+SELECT up.upload_filename, up.upload_timestamp, up.upload_is_nsfw, up.uploader, us.user_name, SUM(vo.vote_number) AS upvotes
+FROM uploads up
+INNER JOIN users us ON up.uploader = us.user_id
+INNER JOIN votes vo ON vo.vote_upload = up.upload_id
+WHERE up.uploader = $1::INT4
+GROUP BY up.upload_filename, up.upload_timestamp, up.upload_is_nsfw, up.uploader, us.user_name;
