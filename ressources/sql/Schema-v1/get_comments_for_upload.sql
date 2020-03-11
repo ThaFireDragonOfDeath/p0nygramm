e94008c0c2d8cec47c764151f30516895c6d5c1b
Voldracarno Draconor <thafiredragonofdeath@gmail.com>
2020-03-11 15:31:23 +0100

    Get upvotes from postgres

--- ressources/sql/Schema-v1/get_comments_for_upload.sql
+++ ressources/sql/Schema-v1/get_comments_for_upload.sql
@@ -1,7 +1,9 @@
 -- Input: upload_id
--- Returns: comment timestamp; comment content; poster (id) and poster (username) ordered by date/time
-SELECT c.comment_timestamp, c.comment_text, c.comment_poster, u.user_name
+-- Returns: comment timestamp; comment content; poster (id), poster (username) and upvotes ordered by date/time
+SELECT c.comment_timestamp, c.comment_text, c.comment_poster, u.user_name, SUM(v.vote_number) AS upvotes
 FROM comments c
 INNER JOIN users u ON c.comment_poster = u.user_id
+INNER JOIN votes v ON c.comment_id = v.vote_comment
 WHERE c.comment_upload = $1::INT4
+GROUP BY c.comment_timestamp, c.comment_text, c.comment_poster, u.user_name
 ORDER BY c.comment_timestamp ASC;
--- ressources/sql/Schema-v1/get_tags_for_upload.sql
+++ ressources/sql/Schema-v1/get_tags_for_upload.sql
@@ -1,8 +1,10 @@
 -- Input: upload_id
--- Returns: comment timestamp; comment content; poster (id) and poster (username) unordered
-SELECT c.comment_timestamp, c.comment_text, c.comment_poster, u.user_name
+-- Returns: tag_text; tag_upvotes; ordered by upvotes
+SELECT ta.tag_text, SUM(vo.vote_number) AS upvotes
 FROM uploads up
 INNER JOIN tag_upload_map tum ON up.upload_id = tum.upload_id
 INNER JOIN tags ta ON tum.tag_id = ta.tag_id
+INNER JOIN votes vo ON tum.tag_id = vo.vote_tagmap
 WHERE tum.upload_id = $1::INT4
-ORDER BY c.comment_timestamp ASC;
+GROUP BY ta.tag_text
+ORDER BY upvotes DESC;
