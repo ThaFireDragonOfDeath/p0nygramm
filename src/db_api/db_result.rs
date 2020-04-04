fdb0d072cf3d5ad939a9f3c308a8e5be34175f0f
Voldracarno Draconor <thafiredragonofdeath@gmail.com>
2020-04-04 17:02:15 +0200

    Change license to Apache-2.0 or MIT

--- src/db_api/db_result.rs
+++ src/db_api/db_result.rs
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2020 Voldracarno Draconor <ThaFireDragonOfDeath@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 use crate::file_api::{get_preview_url_from_filename, get_url_from_filename};
 use chrono::{DateTime, Local};
 use serde::{Serialize, Deserialize};
