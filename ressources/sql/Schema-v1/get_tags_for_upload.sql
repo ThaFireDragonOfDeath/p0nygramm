588ad05af83b6b0b6647f8648e3fec4054dad3cc
Voldracarno Draconor <thafiredragonofdeath@gmail.com>
2020-04-12 21:56:34 +0200

    Fixed error in sql query

--- ressources/sql/Schema-v1/get_tags_for_upload.sql
+++ ressources/sql/Schema-v1/get_tags_for_upload.sql
@@ -4,7 +4,7 @@ SELECT ta.tag_text, SUM(vo.vote_number) AS upvotes
 FROM uploads up
 INNER JOIN tag_upload_map tum ON up.upload_id = tum.upload_id
 INNER JOIN tags ta ON tum.tag_id = ta.tag_id
-INNER JOIN votes vo ON tum.tag_id = vo.vote_tagmap
+INNER JOIN votes_tum vo ON tum.tag_id = vo.vote_tagmap
 WHERE tum.upload_id = $1::INT4
 GROUP BY ta.tag_text
 ORDER BY upvotes DESC;
