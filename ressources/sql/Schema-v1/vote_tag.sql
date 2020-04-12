443da26e1e44f9d653b75ec216d802714471ba9d
Voldracarno Draconor <thafiredragonofdeath@gmail.com>
2020-04-12 16:14:27 +0200

    Improved database schema

--- ressources/sql/Schema-v1/vote_tag.sql
+++ ressources/sql/Schema-v1/vote_tag.sql
@@ -1,5 +1,5 @@
--- Input: upload_id, tag_id, user_id, vote_value TODO
-INSERT INTO votes (vote_tagmap, vote_user, vote_number)
+-- Input: upload_id, tag_id, user_id, vote_value
+INSERT INTO votes_tum (vote_tagmap, vote_user, vote_number)
 VALUES ($1::INT4, $2::INT4, $3::INT4)
 ON CONFLICT (vote_tagmap, vote_user)
 DO UPDATE
