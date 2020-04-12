c0e0b5c47c03ad2aa3da408d1602d91e94f65ca8
Voldracarno Draconor <thafiredragonofdeath@gmail.com>
2020-04-12 22:03:00 +0200

    Fixed error in sql querys

--- ressources/sql/Schema-v1/get_comments_for_upload.sql
+++ ressources/sql/Schema-v1/get_comments_for_upload.sql
@@ -3,7 +3,7 @@
 SELECT c.comment_timestamp, c.comment_text, c.comment_poster, u.user_name, SUM(v.vote_number) AS upvotes
 FROM comments c
 INNER JOIN users u ON c.comment_poster = u.user_id
-INNER JOIN votes v ON c.comment_id = v.vote_comment
+INNER JOIN votes_comments v ON c.comment_id = v.vote_comment
 WHERE c.comment_upload = $1::INT4
 GROUP BY c.comment_timestamp, c.comment_text, c.comment_poster, u.user_name
 ORDER BY c.comment_timestamp ASC;
--- ressources/sql/Schema-v1/get_upload_data.sql
+++ ressources/sql/Schema-v1/get_upload_data.sql
@@ -3,6 +3,6 @@
 SELECT up.upload_filename, up.upload_timestamp, up.upload_is_nsfw, up.uploader, us.user_name, SUM(vo.vote_number) AS upvotes
 FROM uploads up
 INNER JOIN users us ON up.uploader = us.user_id
-INNER JOIN votes vo ON vo.vote_upload = up.upload_id
+INNER JOIN votes_uploads vo ON vo.vote_upload = up.upload_id
 WHERE up.uploader = $1::INT4
 GROUP BY up.upload_filename, up.upload_timestamp, up.upload_is_nsfw, up.uploader, us.user_name;
