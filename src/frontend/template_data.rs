b44cd84a14df36791405e5fe5f88edabe2a86cba
Voldracarno Draconor <thafiredragonofdeath@gmail.com>
2020-06-13 21:20:17 +0200

    Work on template generated frontend

--- src/frontend/template_data.rs
+++ src/frontend/template_data.rs
@@ -1,5 +1,71 @@
 use crate::db_api::db_result::{UploadData, UploadPreview};
 use crate::js_api::response_result::UserData;
+use actix_web::web;
+use actix_session::Session;
+use crate::config::ProjectConfig;
+use crate::db_api::DbConnection;
+use crate::security::get_user_session;
+use crate::js_api::get_filter_ref;
+use crate::db_api::db_result::SessionErrorType::{DbError, SessionExpired, SessionInvalid, NoSession};
+
+// Macros
+macro_rules! get_db_connection {
+    ($config:ident, $req_postgres:expr, $req_redis:expr) => {
+        {
+            let db_connection = DbConnection::new($config.as_ref(), $req_postgres, $req_redis).await;
+
+            if db_connection.is_err() {
+                handle_db_connection_error!(db_connection);
+            }
+
+            db_connection.ok().unwrap()
+        }
+    };
+}
+
+macro_rules! get_user_session_data {
+    ($db_connection:ident, $session:ident, $force_session_renew:expr) => {
+        {
+            let user_session = get_user_session(&$db_connection, &$session, $force_session_renew).await;
+
+            if user_session.is_err() {
+                handle_session_error!(user_session);
+            }
+
+            user_session.ok().unwrap()
+        }
+    };
+}
+
+macro_rules! handle_db_connection_error {
+    ($db_connection:ident) => {
+        let error = $db_connection.err().unwrap();
+        let error_txt = error.error_msg;
+
+        handle_error_str!(error_txt.as_str());
+    };
+}
+
+macro_rules! handle_error_str {
+    ($error_str:expr) => {
+        return UploadViewTemplateData::new_error($error_str);
+    };
+}
+
+macro_rules! handle_session_error {
+    ($user_session:ident) => {
+        let error = $user_session.err().unwrap();
+        let error_type = error.error_type;
+        let error_txt = error.error_msg;
+
+        // If there is no valid session
+        if error_type == SessionExpired || error_type == SessionInvalid || error_type == NoSession {
+            return UploadViewTemplateData::new_empty();
+        }
+
+        handle_error_str!(error_txt.as_str());
+    };
+}
 
 // Helper struct
 #[derive(Clone, Serialize)]
@@ -41,6 +107,7 @@ pub struct UploadViewTemplateData {
 }
 
 impl UploadViewTemplateData {
+    // Used if the user is not logged in
     pub fn new_empty() -> UploadViewTemplateData {
         UploadViewTemplateData {
             backend_data: BackendData::new_default(),
@@ -49,5 +116,26 @@ impl UploadViewTemplateData {
             user_data: None,
             upload_data: None,
         }
+    }
+
+    // Used for backend errors (for example if the database is offline)
+    pub fn new_error(error_msg: &str) -> UploadViewTemplateData {
+        UploadViewTemplateData {
+            backend_data: BackendData::new(true, error_msg),
+            session_settings: None,
+            uploads_prv: Vec::new(),
+            user_data: None,
+            upload_data: None,
+        }
+    }
+
+    // Generate template data for the index view
+    pub async fn new_index(config: &web::Data<ProjectConfig>, session: &Session) -> UploadViewTemplateData {
+        let db_connection = get_db_connection!(config, true, true);
+        let user_session = get_user_session_data!(db_connection, session, false);
+
+        let current_user_id = user_session.user_id;
+
+        return UploadViewTemplateData::new_error("Es ist ein unbekannter Fehler aufgetreten");
     }
 }
