3626031eefcc19fa79f0d88c9e51fe62d2fedb74
Voldracarno Draconor <thafiredragonofdeath@gmail.com>
2020-12-02 19:28:30 +0100

    Fixed SQL type in prepared statement

--- resources/sql/Schema-v1/add_upload.sql
+++ resources/sql/Schema-v1/add_upload.sql
@@ -1,5 +1,5 @@
 -- Input: upload filename, upload is sfw, upload is nsfw, upload type, uploader userid
 -- Returns: upload_id
 INSERT INTO uploads (upload_filename, upload_is_sfw, upload_is_nsfw, upload_type, uploader)
-VALUES ($1::VARCHAR, $2::BOOL, $3::BOOL, $4::UploadType, $5::INT4)
+VALUES ($1::VARCHAR, $2::BOOL, $3::BOOL, $4::ANYENUM, $5::INT4)
 RETURNING upload_id;
